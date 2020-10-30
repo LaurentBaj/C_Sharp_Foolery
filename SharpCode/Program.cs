6cee33d95125541864f76785b305f0ec6049c2ea
LaurentBaj <laurent_baj@hotmail.com>
2020-10-30 22:45:44 +0100

    Implemented Single-Responsibility-Principle on Guessing Game

--- SharpCode/Program.cs
+++ SharpCode/Program.cs
@@ -8,44 +8,10 @@ namespace SharpCode
     {
         static void Main(string[] args)
         {
-            // Create random number
-            int randomNumber = NumberHandler.GenerateRandomNumber(); 
-
-            // Print randomNumber for testing 
-            Console.WriteLine(randomNumber);
-
-            // Store each wrong attempt 
-            ArrayList numbersAlreadyTyped = new ArrayList();
-
-            // Welcome message
             Messages.WelcomeMessage(); 
 
-            int userInput; // Local: so Do-While can reach it 
-
             // while: Number is not within the game rules, typed before, too low or to high
-            do
-            {
-                userInput = Convert.ToInt32(Console.ReadLine()); // user input
-
-                if (userInput < 0 || userInput > 4)
-                {
-                    Messages.NotProperValue(); 
-                }
-                else if (numbersAlreadyTyped.Contains(userInput))
-                {
-                    Messages.AlreadyTypedNumber(); 
-                }
-                else if (userInput < randomNumber)
-                {
-                    Messages.NumberTooSmall(); 
-                    numbersAlreadyTyped.Add(userInput);
-                }
-                else if (userInput > randomNumber)
-                {
-                    Messages.NumberTooLarge(); 
-                    numbersAlreadyTyped.Add(userInput);
-                }
-            } while (userInput != randomNumber);
+            Validate.ValidateInput(NumberHandler.GenerateRandomNumber()); 
 
             Messages.ExitProgram();
         }
--- SharpCode/Validate.cs
+++ SharpCode/Validate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,33 @@ namespace GuessingGame
 {
     class Validate
     {
-        
+        public static void ValidateInput(int randomNumber)
+        {
+            int userInput;
+            ArrayList numbersAlreadyTyped = new ArrayList();
+            do
+            {
+                userInput = Convert.ToInt32(Console.ReadLine());   
+
+                if (userInput < 0 || userInput > 4)
+                {
+                    Messages.NotProperValue();
+                }
+                else if (numbersAlreadyTyped.Contains(userInput))
+                {
+                    Messages.AlreadyTypedNumber();
+                }
+                else if (userInput < randomNumber)
+                {
+                    Messages.NumberTooSmall();
+                    numbersAlreadyTyped.Add(userInput);
+                }
+                else if (userInput > randomNumber)
+                {
+                    Messages.NumberTooLarge();
+                    numbersAlreadyTyped.Add(userInput);
+                }
+            } while (userInput != randomNumber); 
+        }
     }
 }
