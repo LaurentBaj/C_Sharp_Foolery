dfe5cc54d392902160b52551b3360a40163cd4c4
LaurentBaj <laurent_baj@hotmail.com>
2020-11-01 17:21:36 +0100

    Small mod in Program Code
    To make a point

--- DesignPatterns/Singelton/DecoratorImp_Pizzas/Main/Program.cs
+++ DesignPatterns/Singelton/DecoratorImp_Pizzas/Main/Program.cs
@@ -7,17 +7,17 @@ namespace DecoratorImp_Pizzas
     {
         static void Main(string[] args)
         {
-            Ipizza pizzaWithExtraCheese = new PlainPizzza(5, "A pizza");
+            Ipizza plainPizza = new PlainPizzza(5, "A pizza");
 
-            ToppingDecorator p1 = new CheeseTopping(pizzaWithExtraCheese);
-            Ipizza p2 = new JalapenoTopping(pizzaWithExtraCheese);
-            ToppingDecorator p3 = new MushroomTopping(pizzaWithExtraCheese);
+            ToppingDecorator p1 = new CheeseTopping(plainPizza);
+            Ipizza p2 = new JalapenoTopping(plainPizza);
+            ToppingDecorator p3 = new MushroomTopping(plainPizza);
             Ipizza p4 = new PlainPizzza(5.5);
 
-            Console.WriteLine(p1.GetDescription());  
-            Console.WriteLine(p2.GetDescription());  
-            Console.WriteLine(p3.GetDescription());  
-            Console.WriteLine(p4.GetDescription());  
+            Console.WriteLine(p1.GetDescription() + " : " + p1.GetPrice());  
+            Console.WriteLine(p2.GetDescription() + " : " + p2.GetPrice());  
+            Console.WriteLine(p3.GetDescription() + " : " + p3.GetPrice());  
+            Console.WriteLine(p4.GetDescription() + " : " + p4.GetPrice());  
 
             Console.ReadLine(); 
         }
