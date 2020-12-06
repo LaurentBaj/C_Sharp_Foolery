97c874ddb6c399ad1f337e52cd8e6fe6bad4ee71
LaurentBaj <laurent_baj@hotmail.com>
2020-12-06 20:01:14 +0100

    Repetition on recursion
    Did an ex on fibonacci + Factorial

---
+++ Other/Fibonacci/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput; 
+            Console.WriteLine("Enter number");
+
+            userInput = Convert.ToInt32((Console.ReadLine())); 
+
+            Console.WriteLine(Recursive(userInput));
+            Console.WriteLine(Factorial(userInput));
+
+            Console.ReadLine();
+        }
+
+        public static int Recursive(int number)
+        {
+            if (number <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Recursive(number - 1) + Recursive(number - 2);   
+            }
+        }
+
+        public static int Factorial(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+
+
+    }
+}
