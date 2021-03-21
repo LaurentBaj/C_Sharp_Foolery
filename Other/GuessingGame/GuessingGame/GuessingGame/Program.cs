4a098c3744a7a8bbab94a3511af3bdea16cb4c1f
LaurentBaj <laurent_baj@hotmail.com>
2021-03-21 14:18:36 +0100

    Number Guessing Game using Terniary Ops + Recursion

---
+++ Other/GuessingGame/GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int random = new Random().Next(1, 100);
+            int attempts = 0; 
+
+            Console.WriteLine("\nGuess a number between 1 and 100");
+
+            InputProcess(random, attempts);
+        }
+
+        private static void InputProcess(int random, int attempts)
+        {
+            int input = Int32.Parse(Console.ReadLine() ?? string.Empty);
+
+            if (input == random)
+            {
+                Console.WriteLine("Correct!  -  Attempts: " + attempts);
+                return;
+            }
+
+            attempts++; 
+            Console.WriteLine(input < random ? "Too low! Try again!" : "Too high! Try again");
+            InputProcess(random, attempts);
+        }
+        
+    }
+}
